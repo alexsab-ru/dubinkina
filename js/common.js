c66dfd0b29cd3369e99aa92047a5ef4ae152efee
newstreetpunk <newstreetpunk@yandex.ru>
2020-02-08 13:03:41 +0500

    Header

--- js/common.js
+++ js/common.js
@@ -1,3 +1,26 @@
 $(function() {
 
-});
+	if ($(window).width() > 768) {
+
+		var prevScrollpos = window.pageYOffset;
+		window.onscroll = function() {
+			var currentScrollPos = window.pageYOffset;
+			if (prevScrollpos > currentScrollPos) {
+				document.getElementById("header").setAttribute(
+					"style", "top: 0; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);");
+			} else {
+				document.getElementById("header").setAttribute(
+					"style", "top: -100px; box-shadow: none;");
+			}
+			prevScrollpos = currentScrollPos;
+		}
+		$(window).scroll(function() {
+			if ($(this).scrollTop() > 50) {
+				$('.header').attr('style')
+			} else {
+				$('.header').removeAttr('style');
+			}
+		});
+	}
+
+}, jQuery);
